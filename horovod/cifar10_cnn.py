171c7f9be013e5ca200b304ac675823c46f8e100
Stephanie Marker <Stephanie.Marker93@gmail.com>
2018-05-22 11:48:25 -0400

    Remove unneccessary import.

--- horovod/cifar10_cnn.py
+++ horovod/cifar10_cnn.py
@@ -17,7 +17,7 @@ from keras import backend as K
 from keras.datasets import cifar10
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
